4b29af90ab8748b9713811ca6143a755ed8fd984
Randy Fortier <randy.fortier@gmail.com>
2021-08-13 12:07:59 -0400

    Added some clarity to the README

--- lab01_test.py
+++ lab01_test.py
@@ -2,6 +2,7 @@ import lab01
 
 # This function tests your lab 01 code, ensuring that it prints the correct output
 def test_main(capsys):
+    lab01.main() # run the student's code
     captured = capsys.readouterr()
-    assert captured.out == 'Hello, world!'
+    assert captured.out == 'Hello, world!' # check if the output is a match
     # assert lab01.hello_world() == "Hello World!"
